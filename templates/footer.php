188f717c9535ce2edf18fc78175821a7b6c68529
aaroncampbell <aaroncampbell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-23 21:17:04 +0000

    Tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/presenter/trunk@1166416 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/footer.php
@@ -0,0 +1,24 @@
+			</div>
+
+		<?php
+		$presenter_short_url = get_post_meta( get_the_ID(), '_presenter-short-url', true );
+		if ( ! empty( $presenter_short_url ) ) {
+			?>
+			<p class="permalink">
+				<a href="<?php echo esc_attr( $presenter_short_url ); ?>"><?php echo esc_html( $presenter_short_url ); ?></a>
+			</p>
+			<?php
+		}
+		?>
+		</div>
+		<?php
+		/**
+		 * Custom footer action because loading other CSS/JS breaks things
+		 *
+		 * @todo Find a way to still include Analytics codes. At least work with popular GA plugins
+		 */
+		do_action( 'presenter-footer' );
+		?>
+
+	</body>
+</html>
